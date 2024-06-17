e7016b6618e9011fd4d334f346cc5f52f95e6a4e
Perfect26456 <thebhadvaplayer@gmail.com>
2024-06-17 23:08:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -74,6 +74,6 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "http://rishu-web.azurewebsites.net/"
     else:
-        URL = ""
+        URL = "http://rishu-web.azurewebsites.net/"
